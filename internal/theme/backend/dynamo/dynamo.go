ac2ab6192407951910c2696e9dcdcf3764dd8137
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-03-17 23:35:16 +0300

    Refactor create and read theme

--- internal/theme/backend/dynamo/dynamo.go
+++ internal/theme/backend/dynamo/dynamo.go
@@ -7,9 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
-	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 	"github.com/durmusrasit/sencha-restful-api/internal/theme/backend"
-	"github.com/durmusrasit/sencha-restful-api/internal/theme/backend/utils"
 	"github.com/durmusrasit/sencha-restful-api/internal/theme/models"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -25,10 +23,9 @@ func NewDynamoBackend(db dynamodb.DynamoDB) backend.Backend {
 	}
 }
 
-var loggedUserId = "0"
-
 func (b *DynamoBackend) CreateTheme(c *gin.Context) (*models.Theme, error) {
 	var newTheme models.Theme
+	tableName := "themes"
 
 	if c.Bind(&newTheme) == nil {
 		newTheme.ID = uuid.NewString()
@@ -37,13 +34,11 @@ func (b *DynamoBackend) CreateTheme(c *gin.Context) (*models.Theme, error) {
 			return nil, errors.New("Got error marshalling new theme: " + err.Error())
 		}
 
-		tableName := "themes"
+		if newTheme.UserID == "" {
+			return nil, errors.New("userId not given")
+		}
 
-		filt := expression.Name("themeName").Equal(expression.Value(newTheme.ThemeName)).And(expression.Name("userId").Equal(expression.Value(loggedUserId)))
-
-		proj := expression.NamesList(expression.Name("id"), expression.Name("themeName"), expression.Name("backgroundColor"), expression.Name("foregroundColor"), expression.Name("userId"))
-
-		params := utils.GetExprScanInput(tableName, filt, proj)
+		params := GetThemeNameAndUserIdFiltExprScanInput(tableName, newTheme)
 
 		result, err := b.DB.Scan(params)
 		if err != nil {
@@ -63,49 +58,44 @@ func (b *DynamoBackend) CreateTheme(c *gin.Context) (*models.Theme, error) {
 		if err != nil {
 			return nil, errors.New("Got error calling PutItem: " + err.Error())
 		}
-
 	}
 
 	return &newTheme, nil
 }
 
 func (b *DynamoBackend) ReadTheme(c *gin.Context) (*models.Theme, error) {
-	themeName := c.Params.ByName("name")
+	themeName := c.Params.ByName("themeName")
+	userId := c.Params.ByName("userId")
 
 	tableName := "themes"
 
-	filt := expression.Name("themeName").Equal(expression.Value(themeName))
+	data := models.Theme{
+		ThemeName: themeName,
+		UserID:    userId,
+	}
 
-	proj := expression.NamesList(expression.Name("id"), expression.Name("themeName"), expression.Name("backgroundColor"), expression.Name("foregroundColor"), expression.Name("userId"))
-
-	params := utils.GetExprScanInput(tableName, filt, proj)
+	params := GetThemeNameAndUserIdFiltExprScanInput(tableName, data)
 
 	result, err := b.DB.Scan(params)
 	if err != nil {
 		return nil, errors.New("Query API call failed: " + err.Error())
 	}
 
-	getTheme := models.Theme{}
+	if len(result.Items) < 1 {
+		return nil, errors.New("theme not found")
+	}
 
+	var theme models.Theme
 	for _, i := range result.Items {
-		theme := models.Theme{}
 
 		err = dynamodbattribute.UnmarshalMap(i, &theme)
 		if err != nil {
 			panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
 		}
 
-		if theme.UserID == loggedUserId {
-			getTheme.ID = theme.ID
-			getTheme.ThemeName = theme.ThemeName
-			getTheme.BackgroundColor = theme.BackgroundColor
-			getTheme.ForegroundColor = theme.ForegroundColor
-			getTheme.UserID = theme.UserID
-			return &getTheme, nil
-		}
 	}
 
-	return nil, errors.New("theme not found")
+	return &theme, nil
 }
 
 func (b *DynamoBackend) UpdateTheme(c *gin.Context) error {
--- internal/theme/backend/utils/utils.go
+++ internal/theme/backend/utils/utils.go
@@ -1,9 +1,6 @@
 package utils
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 	"github.com/durmusrasit/sencha-restful-api/internal/theme/models"
 )
 
@@ -23,21 +20,4 @@ func IsThemeExistsByName(themes []models.Theme, themeName string) *int {
 		}
 	}
 	return nil
-}
-
-func GetExprScanInput(tableName string, filt expression.ConditionBuilder, proj expression.ProjectionBuilder) *dynamodb.ScanInput {
-	expr, err := expression.NewBuilder().WithFilter(filt).WithProjection(proj).Build()
-	if err != nil {
-		panic("got error building expression: " + err.Error())
-	}
-
-	params := &dynamodb.ScanInput{
-		ExpressionAttributeNames:  expr.Names(),
-		ExpressionAttributeValues: expr.Values(),
-		FilterExpression:          expr.Filter(),
-		ProjectionExpression:      expr.Projection(),
-		TableName:                 aws.String(tableName),
-	}
-
-	return params
 }
