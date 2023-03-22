2de648870681509794cf47da9154b73d5379e8c6
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-03-22 15:51:43 +0300

    Add error handling for empty fields

--- internal/theme/backend/dynamo/utils.go
+++ internal/theme/backend/dynamo/utils.go
@@ -1,6 +1,8 @@
 package dynamo
 
 import (
+	"errors"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
@@ -23,4 +25,18 @@ func GetThemeNameAndUserIdFiltExprScanInput(tableName string, theme models.Theme
 	}
 
 	return scanInput
+}
+
+func CheckColorsAndUserIdGiven(theme models.Theme) error {
+	if theme.BackgroundColor == "" {
+		return errors.New("backgroundColor not given")
+	}
+	if theme.ForegroundColor == "" {
+		return errors.New("foregroundColor not given")
+	}
+	if theme.UserID == "" {
+		return errors.New("userId not given")
+	}
+
+	return nil
 }
